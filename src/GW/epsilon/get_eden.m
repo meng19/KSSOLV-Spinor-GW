f706cb6dc8e2071c6ce8dba98f2cf79680359ce4
mxy <597931375@qq.com>
2026-07-03 17:41:39 +0800

    Update get_eden.m

--- src/GW/epsilon/get_eden.m
+++ src/GW/epsilon/get_eden.m
@@ -10,7 +10,6 @@ if eps.freq_dep == 2
     % 计算频率依赖的情况
     % denominator = 2 / (1 / (freq + energy_diff) + 1 / (-freq + energy_diff));
     % denominator = (energy_diff^2 - freq^2) / energy_diff;
-    % eden = 1 / sqrt(denominator); 使用sqrt乘以每个gme会造成误差？
     eden = energy_diff / (energy_diff^2 - freq^2);
 elseif eps.freq_dep == 0
     eden = 1/(eval-econd);
